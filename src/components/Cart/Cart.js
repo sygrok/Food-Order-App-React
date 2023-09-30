4d7558658e674683680a6c973a0fbdf45d86bd3c
Berkay Anduv <sygrok@hotmail.com>
2023-09-30 06:32:45 +0300

    Portal&Modal

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,8 +1,9 @@
+import Modal from "../UI/Modal";
 import classes from "./Cart.module.css";
 
 const Cart = (props) => {
   const cartItems = (
-    <ul className={classes.['cart-items']}>
+    <ul className={classes["cart-items"]}>
       {[{ id: "c1", name: "sushi", amount: "2", price: "12.99" }].map((x) => (
         <li>{x.name}</li>
       ))}
@@ -10,13 +11,19 @@ const Cart = (props) => {
   );
 
   return (
-    <div>
+    <Modal onClose={props.onClose}>
       {cartItems}
-      <div className={classes.total}><span>35.68</span></div>
+      <div className={classes.total}>
+        <span>35.68</span>
+      </div>
       <div className={classes.actions}>
-        <button className={classes['button--alt']}>Close</button>
+        <button className={classes["button--alt"]} onClick={props.onClose}>
+          Close
+        </button>
         <button className={classes.button}>Order</button>
       </div>
-    </div>
+    </Modal>
   );
 };
+
+export default Cart;
--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -7,7 +7,7 @@ const Header = (props) => {
     <>
       <header className={classes.header}>
         <h1>ReactMeals</h1>
-        <HeaderCartButton />
+        <HeaderCartButton onClick={props.onShowCart} />
       </header>
       <div className={classes.main_image}>
         <img src={meal} alt="ReactMeals"></img>
